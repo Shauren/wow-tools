54252bb2dc2cb7bce33cf60b043d0ded64a3e599
Shauren <shauren.trinity@gmail.com>
2019-10-19 21:12:55 +0200

    8.2.0 fixups

--- UpdateFieldCodeGenerator/Structures/CGAzeriteItemData.cs
+++ UpdateFieldCodeGenerator/Structures/CGAzeriteItemData.cs
@@ -9,7 +9,7 @@
         public static readonly UpdateField m_level = new UpdateField(typeof(uint), UpdateFieldFlag.Owner);
         public static readonly UpdateField m_auraLevel = new UpdateField(typeof(uint), UpdateFieldFlag.Owner);
         public static readonly UpdateField m_knowledgeLevel = new UpdateField(typeof(uint), UpdateFieldFlag.Owner);
-        public static readonly UpdateField m_DEBUGknowledgeWeek = new UpdateField(typeof(uint), UpdateFieldFlag.Owner);
+        public static readonly UpdateField m_DEBUGknowledgeWeek = new UpdateField(typeof(int), UpdateFieldFlag.Owner);
         public static readonly UpdateField m_unlockedEssences = new UpdateField(typeof(DynamicUpdateField<JamMirrorUnlockedAzeriteEssence_C>), UpdateFieldFlag.None);
         public static readonly UpdateField m_selectedEssences = new UpdateField(typeof(DynamicUpdateField<JamMirrorSelectedAzeriteEssences_C>), UpdateFieldFlag.None);
         public static readonly UpdateField m_unlockedEssenceMilestones = new UpdateField(typeof(DynamicUpdateField<uint>), UpdateFieldFlag.None);
--- UpdateFieldCodeGenerator/Structures/JamMirrorSelectedAzeriteEssences_C.cs
+++ UpdateFieldCodeGenerator/Structures/JamMirrorSelectedAzeriteEssences_C.cs
@@ -4,7 +4,7 @@
     public class JamMirrorSelectedAzeriteEssences_C
     {
         public static readonly UpdateField m_azeriteEssenceID = new UpdateField(typeof(uint[]), UpdateFieldFlag.None, 3);
-        public static readonly UpdateField field_0 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_specializationID = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_enabled = new UpdateField(typeof(Bits), UpdateFieldFlag.None, bitSize: 1);
     }
 }
