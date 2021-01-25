2f4321928b2ca5e597c7bd7ea799dbccad9f0ef7
Shauren <shauren.trinity@gmail.com>
2021-01-25 16:11:03 +0100

    Correct CGCorpseData fields

--- UpdateFieldCodeGenerator/Structures/CGCorpseData.cs
+++ UpdateFieldCodeGenerator/Structures/CGCorpseData.cs
@@ -11,9 +11,9 @@
         public static readonly UpdateField m_guildGUID = new UpdateField(typeof(WowGuid), UpdateFieldFlag.None);
         public static readonly UpdateField m_displayID = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_items = new UpdateField(typeof(uint[]), UpdateFieldFlag.None, 19);
-        public static readonly UpdateField m_unused = new UpdateField(typeof(byte), UpdateFieldFlag.None);
         public static readonly UpdateField m_raceID = new UpdateField(typeof(byte), UpdateFieldFlag.None);
         public static readonly UpdateField m_sex = new UpdateField(typeof(byte), UpdateFieldFlag.None);
+        public static readonly UpdateField m_class = new UpdateField(typeof(byte), UpdateFieldFlag.None);
         public static readonly UpdateField m_customizations = new UpdateField(typeof(DynamicUpdateField<JamMirrorChrCustomizationChoice_C>), UpdateFieldFlag.None);
         public static readonly UpdateField m_flags = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_factionTemplate = new UpdateField(typeof(int), UpdateFieldFlag.None);
