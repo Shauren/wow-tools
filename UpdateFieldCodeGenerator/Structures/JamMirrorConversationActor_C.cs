9d593508dd5c40c341348da44673959f4ded1796
Shauren <shauren.trinity@gmail.com>
2021-03-07 12:41:43 +0100

    Name unknown Conversation fields

--- UpdateFieldCodeGenerator/Structures/JamMirrorConversationActor_C.cs
+++ UpdateFieldCodeGenerator/Structures/JamMirrorConversationActor_C.cs
@@ -5,7 +5,7 @@
         public static readonly UpdateField m_creatureID = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_creatureDisplayInfoID = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_actorGUID = new UpdateField(typeof(WowGuid), UpdateFieldFlag.None);
-        public static readonly UpdateField field_18 = new UpdateField(typeof(int), UpdateFieldFlag.None);
+        public static readonly UpdateField m_id = new UpdateField(typeof(int), UpdateFieldFlag.None);
         public static readonly UpdateField m_type = new UpdateField(typeof(Bits), UpdateFieldFlag.None, bitSize: 1);
         public static readonly UpdateField m_noActorObject = new UpdateField(typeof(Bits), UpdateFieldFlag.None, bitSize: 1);
     }
