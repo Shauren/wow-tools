28d051cad6e96322b3a1f41782e2dffa4a861dec
Shauren <shauren.trinity@gmail.com>
2021-07-29 18:16:46 +0200

    Name JamMirrorPVPInfo_C fields

--- UpdateFieldCodeGenerator/Structures/JamMirrorPVPInfo_C.cs
+++ UpdateFieldCodeGenerator/Structures/JamMirrorPVPInfo_C.cs
@@ -3,14 +3,14 @@
     [HasChangesMask]
     public class JamMirrorPVPInfo_C
     {
-        public static readonly UpdateField field_0 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_4 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_8 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_C = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_weeklyPlayed = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_weeklyWon = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_seasonPlayed = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_seasonWon = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_rating = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_14 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_18 = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_weeklyBestRating = new UpdateField(typeof(uint), UpdateFieldFlag.None);
+        public static readonly UpdateField m_seasonBestRating = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_pvpTierID = new UpdateField(typeof(uint), UpdateFieldFlag.None);
-        public static readonly UpdateField field_20 = new UpdateField(typeof(bool), UpdateFieldFlag.None, bitSize: 1);
+        public static readonly UpdateField m_disqualified = new UpdateField(typeof(bool), UpdateFieldFlag.None, bitSize: 1);
     }
 }
