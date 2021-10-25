317d162197cf7c3fecb0cdfa0f0e2d6b3f08c2f8
ModoX <moardox@gmail.com>
2021-10-25 09:54:53 +0200

    Made conversation time fields viewer dependent  (#5)

--- UpdateFieldCodeGenerator/Structures/CGConversationData.cs
+++ UpdateFieldCodeGenerator/Structures/CGConversationData.cs
@@ -8,7 +8,7 @@ namespace UpdateFieldCodeGenerator.Structures
         public static readonly ObjectType ObjectType = ObjectType.Conversation;
 
         public static readonly UpdateField m_linesSize = new UpdateField(typeof(BlzVectorField<JamMirrorConversationLine_C>), UpdateFieldFlag.None, typeof(CGConversationData).GetField("m_lines", BindingFlags.Static | BindingFlags.Public));
-        public static readonly UpdateField m_lastLineEndTime = new UpdateField(typeof(int), UpdateFieldFlag.None);
+        public static readonly UpdateField m_lastLineEndTime = new UpdateField(typeof(int), UpdateFieldFlag.None, customFlag: CustomUpdateFieldFlag.ViewerDependent);
         public static readonly UpdateField m_progress = new UpdateField(typeof(uint), UpdateFieldFlag.None);
         public static readonly UpdateField m_lines = new UpdateField(typeof(BlzVectorField<JamMirrorConversationLine_C>), UpdateFieldFlag.None);
         public static readonly UpdateField m_dontPlayBroadcastTextSounds = new UpdateField(typeof(bool), UpdateFieldFlag.None, bitSize: 1);
