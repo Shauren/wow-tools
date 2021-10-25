ae5382b731ee4b206ea974f4f6a92cbb0e438080
Shauren <shauren.trinity@gmail.com>
2021-10-25 09:54:32 +0200

    Apply suggestions from code review

--- UpdateFieldCodeGenerator/Structures/JamMirrorConversationLine_C.cs
+++ UpdateFieldCodeGenerator/Structures/JamMirrorConversationLine_C.cs
@@ -3,7 +3,7 @@
     public class JamMirrorConversationLine_C
     {
         public static readonly UpdateField m_conversationLineID = new UpdateField(typeof(int), UpdateFieldFlag.None);
-        public static readonly UpdateField m_startTime = new UpdateField(typeof(uint), UpdateFieldFlag.None, 0, 0, CustomUpdateFieldFlag.ViewerDependent);
+        public static readonly UpdateField m_startTime = new UpdateField(typeof(uint), UpdateFieldFlag.None, customFlag: CustomUpdateFieldFlag.ViewerDependent);
         public static readonly UpdateField m_uiCameraID = new UpdateField(typeof(int), UpdateFieldFlag.None);
         public static readonly UpdateField m_actorIndex = new UpdateField(typeof(byte), UpdateFieldFlag.None);
         public static readonly UpdateField m_flags = new UpdateField(typeof(byte), UpdateFieldFlag.None);
