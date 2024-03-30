f945554defb39540599aac3984cbbe74764afbec
ModoX <moardox@gmail.com>
2024-03-30 20:04:50 +0100

    Name MawPower fields

--- UpdateFieldCodeGenerator/Structures/JamMirrorMawPower_C.cs
+++ UpdateFieldCodeGenerator/Structures/JamMirrorMawPower_C.cs
@@ -2,8 +2,8 @@
 {
     public class JamMirrorMawPower_C
     {
-        public static readonly UpdateField field_0 = new UpdateField(typeof(int), UpdateFieldFlag.None);
-        public static readonly UpdateField field_4 = new UpdateField(typeof(int), UpdateFieldFlag.None);
-        public static readonly UpdateField field_8 = new UpdateField(typeof(int), UpdateFieldFlag.None);
+        public static readonly UpdateField SpellID = new UpdateField(typeof(int), UpdateFieldFlag.None);
+        public static readonly UpdateField MawPowerID = new UpdateField(typeof(int), UpdateFieldFlag.None);
+        public static readonly UpdateField Stacks = new UpdateField(typeof(int), UpdateFieldFlag.None);
     }
 }
